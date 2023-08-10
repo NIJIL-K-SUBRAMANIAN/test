f2e101f61e50bec89db11088746055df1dd1e3d3
nijil k s <nijil4445@gmail.com>
2023-08-10 11:53:00 +0530

    project page

--- src/assets/linkholder/navcomponents/OrganicProducts.jsx
+++ src/assets/linkholder/navcomponents/OrganicProducts.jsx
@@ -9,20 +9,35 @@ import { useState,useEffect } from 'react';
 
 function OrganicProducts() {
 
-  const [apiImage,setApiImage] = useState();
+  const [apiImage,setApiImage] = useState([]);
+
   useEffect(() => {
-    axios.get('https://api.unsplash.com/search/photos?page=1&query=spa&client_id=gMxi3sjwT4j1qTG24nNDHjjqNtvisrDaoyAtv9nPs2I')
+    axios.get('https://api.unsplash.com/search/photos?page=1&query=oil&client_id=gMxi3sjwT4j1qTG24nNDHjjqNtvisrDaoyAtv9nPs2I')
       .then((response) => {
        
         setApiImage(response.data.results);
       })
-      
+        
   }, []);
-  console.log(apiImage);
+
   return (
     <div className='organicproducts'>
+      <div>
       <h1 style={{textAlign:'center'}}>BOUTIQUE & SPA PRODUCTS</h1>
-      
+     <div className='wrapper'>
+     {
+          apiImage.map((itm,key)=>{
+            return (
+              <div className=''>
+                <img src={itm.urls.raw} alt="" style={{width:"15rem",height:"15rem"}} />
+                <p style={{fontSize:"small"}}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas accusantium inventore, quis neque </p>
+                <button className='button' style={{marginLeft:"4px"}}>ADD TO CART</button>
+              </div>
+            )
+          })
+     }
+     </div>
+     </div>
     </div>
   )
 }
